b634498dc6e02630298e392f7b1711d64b02675c
Cloud User <ec2-user@ip-172-31-7-144.ap-south-1.compute.internal>
2022-07-05 08:44:10 +0000

    dev branch commit

--- abc.java
+++ abc.java
@@ -2,3 +2,5 @@ This file contains DB connection logics
 this line is added
 s line is also added
 hi hlo hw are you
+this line is updated in development branch
+this line is updated in development branch
